0791486f7fc34a581e42a44c7fee3577a0694b0d
somb1 <somersbyappl@gmail.com>
2025-12-25 00:12:40 +0900

    Edit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
-  modules: ['@nuxt/ui'],
-  css: [
-    '~/assets/css/main.css',
-    'maplibre-gl/dist/maplibre-gl.css'
-  ],
-  routeRules: {
-    '/api/eew': { headers: { 'Cache-Control': 'max-age=3600' } },
-  },
-})
+	compatibilityDate: "2025-07-15",
+	devtools: { enabled: true },
+	modules: ["@nuxt/ui"],
+	css: ["~/assets/css/main.css", "maplibre-gl/dist/maplibre-gl.css"],
+	routeRules: {
+		"/api/eew": { headers: { "Cache-Control": "public, s-maxage=60" } },
+	},
+});
