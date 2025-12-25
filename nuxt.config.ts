1780f056d0426bdba5d3a17ddc13d399f9f5bd18
somb1 <somersbyappl@gmail.com>
2025-12-26 02:55:09 +0900

    Edit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,6 +11,6 @@ export default defineNuxtConfig({
 		"/api/realtime_points": {
 			headers: { "Cache-Control": "public, s-maxage=60, maxage=60" },
 		},
-		"/api/latest": { swr: 1 },
+		"/api/latest": { swr: true, headers: { "Cache-Control": "public, s-maxage=1, maxage=1" } },
 	},
 });
