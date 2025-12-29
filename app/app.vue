7332cb88510e270a157d5ed9f7478e0de4597081
somb1 <somersbyappl@gmail.com>
2025-12-29 13:26:56 +0900

    Edit

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,11 @@
 <template>
 	<UApp>
+		<Analytics/>
 		<AppHeader />
 		<NuxtPage />
 	</UApp>
 </template>
+
+<script setup lang="ts">
+	import { Analytics } from '@vercel/analytics/nuxt'
+</script>
